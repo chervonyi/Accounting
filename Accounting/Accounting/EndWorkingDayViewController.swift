6d897f05ae6f1fe42fd9d0cdd7befab6d4871663
chervonyi <yuri.ternopil@gmail.com>
2018-10-23 14:03:24 -0500

    Added EndWorkingDay scene

---
+++ Accounting/Accounting/EndWorkingDayViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EndWorkingDayViewController.swift
+//  Accounting
+//
+//  Created by Yuri Chervonyi on 10/23/18.
+//  Copyright © 2018 Red Inc. All rights reserved.
+//
+
+import UIKit
+
+class EndWorkingDayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
