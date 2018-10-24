c34cb4d2c479ffc632b5679f0f766d7142676ba6
chervonyi <yuri.ternopil@gmail.com>
2018-10-23 23:17:06 -0500

    Added StatisticsDay scene

---
+++ Accounting/Accounting/StatisticsDayViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsDayViewController.swift
+//  Accounting
+//
+//  Created by Yuri Chervonyi on 10/23/18.
+//  Copyright © 2018 Red Inc. All rights reserved.
+//
+
+import UIKit
+
+class StatisticsDayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
